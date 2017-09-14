8c525e78e1670ef4a4e7eb17f028a17eb1599ba1
minzzzy <minpjh@gmail.com>
2017-09-14 17:02:35 +0900

    Update simple_nn.py

--- simple_nn.py
+++ simple_nn.py
@@ -1,14 +1,14 @@
 import numpy as np
 from miniflow import *
-import pprint
+from pprint import pprint
 
 DEBUG = 0
 
-X, W, b = Input(), Input(), Input()
-y = Input()
-l = Linear(X, W, b)
-s = Sigmoid(l)
-cost = MSE(y, s)
+X, W, b = Input('X'), Input('W'), Input('b')
+y = Input('y')
+l = Linear(X, W, b, 'l')
+s = Sigmoid(l,'s')
+cost = MSE(y, s, 'cost')
 
 X_ = np.array([[-1., -2.], [-1, -2]])
 W_ = np.array([[2.],[3.]])
@@ -29,27 +29,17 @@ forward_and_backward(graph)
 gradients = [t.gradients[t] for t in [X, y, W, b]]
 
 if (DEBUG):
-    print("< Nodes >")
-    print("- X : ", X)
-    print("- W : ", W)
-    print("- b : ", b)
-    print("- y : ", y)
-    print("- l : ", l)
-    print("- Sigmoid : ", s)
-    print("- cost : ", cost)
-    print("")
 
     print("< Graph >")
-    pprint.pprint(graph)
-    print("")
+    for n in graph:
+        print(n.name, " : ", n)
 
     print("< feed_dict >")
-    print("- X : ", X.value)
-    print("- W : ", W.value)
-    print("- b : ", b.value)
-    print("- y : ", y.value)
-    print("")
+    print(X.name, " : ", X.value)
+    print(W.name, " : ", W.value) 
+    print(b.name, " : ", b.value)
+    print(y.name, " : ", y.value) 
 
-    print("- COST : ", cost.value)
+    print(cost.name, " : ", cost.value) 
     print("- GRADIENTS in Input : ")
     pprint.pprint(gradients)
